f70aa98990f3f223d5095cf11d1b520e84164608
thetotobalgo <116635056+thetotobalgo@users.noreply.github.com>
2022-11-07 16:39:40 +0000

    push09

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,45 @@
-$('#work').click(function (e) { 
+const article1 = document.getElementsByClassName('article01');
+const article2 = document.getElementsByClassName('article02');
+const article3 = document.getElementsByClassName('article03');
+
+var pageactive = 1;
+
+$('#work').click(function (e) {
     e.preventDefault();
-    $('.article01').show();
-    $('.article02').hide();
-    $('.article03').hide();
+
+    if (pageactive == 2) {
+        article1[0].style.transform = "translateX(-100%)";
+        article2[0].style.transform = "translateX(-100%)";
+        article3[0].style.transform = "translateX(-100%)";
+    }
+    if (pageactive == 3)
+    {
+        article1[0].style.transform = "translateX(-200%)";
+        article2[0].style.transform = "translateX(-200%)";
+        article3[0].style.transform = "translateX(-200%)";
+    }
+    pageactive = 1;
+
 });
 
-$('#about').click(function (e) { 
+$('#about').click(function (e) {
     e.preventDefault();
-    $('.article01').hide();
-    $('.article02').show();
-    $('.article03').hide();
+    if (pageactive == 1) {
+        article1[0].style.transform = "translateX(100%)";
+        article2[0].style.transform = "translateX(100%)";
+        article3[0].style.transform = "translateX(100%)";
+    }
+    if (pageactive == 3)
+    {
+        article1[0].style.transform = "translateX(-100%)";
+        article2[0].style.transform = "translateX(-100%)";
+        article3[0].style.transform = "translateX(-100%)";
+    }
+    pageactive = 2;
 });
 
-$('#contact').click(function (e) { 
+$('#contact').click(function (e) {
     e.preventDefault();
-    $('.article01').hide();
-    $('.article02').hide();
-    $('.article03').show();
+
 });
 
