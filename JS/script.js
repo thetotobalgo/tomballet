1a79c44ccb3f9e8ca7abb201837084fc45defb00
thetotobalgo <116635056+thetotobalgo@users.noreply.github.com>
2022-11-14 17:27:49 +0000

    de

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,9 @@
-const article1 = document.getElementsByClassName('article01');
-const article2 = document.getElementsByClassName('article02');
-const article3 = document.getElementsByClassName('article03');
+function page(page) {
+    $('.section-main').hide();
+    $(page).show();
+}
 
-var pageactive = 1;
-
-$('#work').click(function (e) {
-    e.preventDefault();
-    $('.article01').show();
-    $('.article02').hide();
-    $('.article03').hide();
+$(document).ready(function() {
+    $('.section-main').hide();
+    $('#section-about').show();
 });
-
-$('#about').click(function (e) {
-    e.preventDefault();
-    $('.article01').hide();
-    $('.article02').show();
-    $('.article03').hide();
-});
-
-$('#contact').click(function (e) {
-    e.preventDefault();
-    $('.article01').hide();
-    $('.article02').hide();
-    $('.article03').show();
-});
-
