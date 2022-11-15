93bb58ee2d01344ae0a04d2763cde0da03d9adfa
thetotobalgo <116635056+thetotobalgo@users.noreply.github.com>
2022-11-15 21:09:43 +0000

    e

--- JS/script.js
+++ JS/script.js
@@ -8,31 +8,24 @@ $(document).ready(function () {
     $('#section-home').show();
 });
 
-
-var hits = 0;
 var swi = 0;
-
 var nom = document.querySelector('.nom');
 
-document.body.onkeyup = function (e) {
-    if (e.keyCode == 32) {
-        switch (swi) {
-            case 0:
-                nom.innerHTML = "Tom";
-                swi++;
-                break;
-            case 1:
-                nom.innerHTML = "Tom Ballet";
-                swi++;
-                break;
-            case 2:
-                nom.innerHTML = "トム";
-                swi=0;
-                break;
-
-        }
-
+$('.bubble-wrapper').click(function (e) { 
+    e.preventDefault();
+    switch (swi) {
+        case 0:
+            nom.innerHTML = "Tom";
+            swi++;
+            break;
+        case 1:
+            nom.innerHTML = "Tom Ballet";
+            swi++;
+            break;
+        case 2:
+            nom.innerHTML = "トム";
+            swi=0;
+            break;
 
     }
-
-}
+});
